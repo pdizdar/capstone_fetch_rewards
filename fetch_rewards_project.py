bdb9736e2fe0176709005737957f2049153e7d11
Pratima Dizdar <pratima.dizdar@gmail.com>
2022-10-06 23:15:33 -0400

    initial commit for capstone project

---
+++ fetch_rewards_project.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv (r'C:\Users\prati\OneDrive\Desktop\data class\capstone_project\FETCH REWARDS SUMMARY.csv')
+print(df)    #read and print the csv file
+
+
+
+df.columns
+print(columns)
